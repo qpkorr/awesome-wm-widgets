e8e9c215c8c0443b61242982f8f89f4a3b04c96b
John Orr <j3dt>
2020-07-20 12:21:56 +1000

    Allow wider cpu-widget popup to avoid long process names causing some kind of line wrapping which was changing the height of the popup

--- cpu-widget/cpu-widget.lua
+++ cpu-widget/cpu-widget.lua
@@ -113,8 +113,8 @@ local function worker(args)
         shape = gears.shape.rounded_rect,
         border_width = 1,
         border_color = beautiful.bg_normal,
-        maximum_width = 300,
-        offset = { y = 5 },
+        maximum_width = 400,
+        offset = { y = 0 },
         widget = {}
     }
 
